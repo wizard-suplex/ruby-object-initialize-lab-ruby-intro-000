605a4a4fd6f6d11d53fee9e37ed28efe95b7981a
Alan Cooke <atc4@rice.edu>
2021-01-20 19:47:38 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+end
+
+end
